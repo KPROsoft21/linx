cb67654786bffe87f1aafaa4a9fdd13ba48091da
GrandDataMaster <99348571+KPROsoft21@users.noreply.github.com>
2024-02-10 18:34:21 +0300

    Update settings.py

--- project4/settings.py
+++ project4/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '13kl@xtukpwe&xj2xoysxe9_6=tf@f8ewxer5n&ifnd46+6$%8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
